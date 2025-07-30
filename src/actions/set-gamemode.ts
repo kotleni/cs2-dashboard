e0eeff489b53185818da06fecbb32fa37bfb2d12
Viktor Varenik <yavarenikya@gmail.com>
2025-07-30 12:13:57 +0300

    feat: preserve map when change gamemode

--- src/actions/set-gamemode.ts
+++ src/actions/set-gamemode.ts
@@ -6,13 +6,21 @@ const HOST = 'kotleni.pp.ua';
 const PORT = 27015;
 const PASSWORD = 'password1K';
 
+async function getMapName(rconService: RCONService) {
+    const result = await rconService.exec('status');
+    const secondPart = result.split('spawngroup(  1)  : SV:  [1: ')[1];
+    return secondPart.split(' ')[0];
+}
+
 export async function setGameMode(gamemodeName: string): Promise<string> {
     console.log('set gamemode to ' + gamemodeName);
     const rconService = new RCONService(HOST, PORT, PASSWORD);
     await rconService.connect();
     const result = await rconService.exec('game_alias ' + gamemodeName);
     await rconService.exec('say Changing gamemode...');
-    await rconService.exec('changelevel de_mirage'); // TODO
+    const mapName = await getMapName(rconService);
+    console.log('preserved map ' + mapName);
+    await rconService.exec('changelevel ' + mapName);
     await rconService.disconnect();
     return result;
 }
